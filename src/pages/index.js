b9b0204aa14be0c750fa91ea9ae58a83c2b87624
blue mainPC <mainPC@mail.com>
2023-01-27 22:09:12 -0700

    updated a npm package vulnerability

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
 
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Card from "react-bootstrap/Card";
 
 export default function Index() {
     return (
         <Container>
+
             <section className="text-center text-md-start mb-5">
                 <h1 className="display-2">Next-Bootstrap</h1>
                 <p className="text-muted">
                     Quickly create a project with Next Js and Bootstrap v5.2
                 </p>
             </section>
+
         </Container>
     )
 }
